218628e714c8daea2192aa710084e7c0a1c25dda
Anmakulaku <anmakulaku@gmail.com>
2023-11-27 14:29:54 +0100

    Add counter

--- app.js
+++ app.js
@@ -5,41 +5,51 @@ let shuffleEmoticons = [...emoticons].sort(() => (Math.random() - 0.5));
 const gameContent = document.querySelector('.game__content');
 const modal = document.querySelector('.modal');
 const closeModalButton = document.querySelector('.closeModal');
+const turnsCounter = document.getElementById('turnsCounter');
+
+let clickCount = 0;
+let openCards = [];
 
 closeModalButton.addEventListener('click', () => {
     modal.classList.add('hidden');
 });
 
-for (let i=0; i<emoticons.length; i++) {
+gameContent.addEventListener('click', (event) => {
+    const clickedElement = event.target;
+
+    if (clickedElement.classList.contains('item') && !clickedElement.classList.contains('boxOpen') && openCards.length < 2) {
+        clickedElement.classList.add('boxOpen');
+        openCards.push(clickedElement);
+        if (openCards.length === 2) {
+            clickCount++;
+            turnsCounter.textContent = clickCount;
+            setTimeout(() => checkMatch(), 300);
+        }
+    }
+});
+
+function checkMatch() {
+    const [firstCard, secondCard] = openCards;
+    if (firstCard.innerHTML === secondCard.innerHTML) {
+        openCards.forEach((item) => {
+            item.classList.add('boxMatched');
+        });
+
+        openCards = [];
+        if (document.querySelectorAll(".boxMatched").length === shuffleEmoticons.length) {
+            modal.classList.remove('hidden');
+        }
+    } else {
+        openCards.forEach((item) => {
+            item.classList.remove('boxOpen');
+        });
+        openCards = [];
+    }
+}
+
+for (let i = 0; i < emoticons.length; i++) {
     let box = document.createElement('div');
     box.className = 'item';
     box.innerHTML = shuffleEmoticons[i];
-
-    box.onclick = function() {
-        this.classList.add('boxOpen')
-        const openCards = document.querySelectorAll('.boxOpen');
-        setTimeout(() => {
-            if (openCards.length <=2) {
-                if (openCards[0].innerHTML === openCards[1].innerHTML) {
-                    openCards.forEach((item) => {
-                        item.classList.add('boxMatched');
-                        // console.log('Znaleziono parę!');
-                    });
-                    openCards.forEach((item) => {
-                        item.classList.remove('boxOpen');
-                        // console.log('Usunięto klasę boxOpen');
-                    });
-                    if(document.querySelectorAll(".boxMatched").length === shuffleEmoticons.length) {
-                        modal.classList.remove('hidden');
-                    } 
-                }  else {
-                    openCards.forEach((item) => {
-                        item.classList.remove('boxOpen');
-                        // console.log('Nie wiadomo');
-                    });
-                }
-            } 
-        }, 300);
-    }
     gameContent.appendChild(box);
-};
+}
